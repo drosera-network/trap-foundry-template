7a13afbadf6379e462337c881ae3e792853aed92
0xkr8os <kratos@groomla.ke>
2023-12-05 12:41:00 -0600

    update trap constructor and readme

--- test/UniswapV3PoolTrap.t.sol
+++ test/UniswapV3PoolTrap.t.sol
@@ -5,7 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {UniswapV3PoolTrap} from "../src/UniswapV3PoolTrap.sol";
 
 contract UniswapV3PoolTrapTest is Test {
-    uint8 numBlocks = 3;
+    uint8 numBlocks = 5;
     uint256 blockNumber;
 
     function setUp() public {
@@ -18,15 +18,14 @@ contract UniswapV3PoolTrapTest is Test {
 
       // Collect data points starting from the current block minus 'numBlocks'
       for (uint8 i = 0; i < numBlocks; i++) {
-        dataPoints[i] = _deployTrap().collect();
+        dataPoints[i] = new UniswapV3PoolTrap().collect();
+
+        // Advance to the next block
         vm.rollFork(blockNumber - numBlocks + i);
       }
 
-      _deployTrap().isValid(dataPoints);
-    }
-
-    function _deployTrap() public returns (UniswapV3PoolTrap) {
-      address ethUsdcPool = 0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
-      return new UniswapV3PoolTrap(ethUsdcPool);
+      bool isValid = new UniswapV3PoolTrap().isValid(dataPoints);
+      
+      assertTrue(isValid);
     }
 }
